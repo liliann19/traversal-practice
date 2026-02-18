25fa0795c4eb749f363f50baaadb6b0612a70e0a
Lilian <210286008+liliann19@users.noreply.github.com>
2026-02-17 20:16:43 -0800

    implemented treeSum

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -47,7 +47,8 @@ public class TraversalPractice {
    * @return the sum
    */
   public static int treeSum(Node<Integer> node) {
-    return 0;
+    if (node == null) return 0;
+    return treeSum(node.left) + treeSum(node.right) + node.value;
   }
 
   /**
